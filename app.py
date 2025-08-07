7c6063075d11c0c262d6585d7491d12036e5a58d
Adlin Hamdan <afhamdan@wisc.edu>
2025-08-07 17:22:25 -0500

    update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def api():
         return jsonify({"response": f"Failed to get response! {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(port=9000)
+    app.run(port=9000, host='0.0.0.0')
     
     
 
